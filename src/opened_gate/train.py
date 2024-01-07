568973b83b93bc8d9b2154c7d584258f48ea81eb
Rafał Nowak <rafal.nowak@tooploox.com>
2024-01-07 20:37:28 +0100

    deploy

--- src/opened_gate/train.py
+++ src/opened_gate/train.py
@@ -154,24 +154,6 @@ def main():
 
     # Save the model
     trainer.save_checkpoint("data/06_models/model.ckpt")
-    # trainer.save_checkpoint("s3://opened-gate/models/model.ckpt")
-
-    # Convert to ONNX
-    input_sample = torch.randn(1, 3, 224, 224)
-    torch.onnx.export(
-        model,
-        input_sample,
-        "app/model.onnx",
-        export_params=True,
-        opset_version=11,
-        do_constant_folding=True,
-        input_names=["input"],
-        output_names=["output"],
-        dynamic_axes={
-            "input": {0: "batch_size"},
-            "output": {0: "batch_size"},
-        },
-    )
 
 
 if __name__ == "__main__":
