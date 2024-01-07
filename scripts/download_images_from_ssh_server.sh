48fbe39b2c3308e842e4696ee53191a20d7a2ca4
Rafał Nowak <rnoxy@users.noreply.github.com>
2024-01-07 13:27:46 +0100

    initial commit (#1)
    * initial commit
    
    * add script to download images from ssh
    
    * fix "3.10"
    
    * ignore envrc
    
    * ignore .idea
    
    * add lightnine and omegaconf
    
    * rename from "main"
    
    * black
    
    * add boto3 and s3fs
    
    * dvc init
    
    * ignore models and lightning logs

---
+++ scripts/download_images_from_ssh_server.sh
@@ -0,0 +1,36 @@
+# Script to download the camera images from the SSH server.
+# The script is using SSH to connect to the server and download the images.
+# The script is using the `rsync` command to download the images.
+# Warning: The script is using the `rsync` command with the `--remove-source-files` option.
+#          This option will remove the source files after the download.
+#          This option is used to avoid downloading the same images again.
+
+#
+# The script is using the following environment variables:
+#   - DEST_LOCATION: The destination location where the images will be downloaded.
+#   - SSH_SERVER: The SSH server to connect to.
+#   - SSH_USER: The SSH user to use to connect to the server.
+#   - SOURCE_LOCATION: The source location where the images are located.
+#   - OPENED_IMAGES_PATTERN: The pattern to use to download the opened images.
+#   - CLOSED_IMAGES_PATTERN: The pattern to use to download the closed images.
+
+# The opened and closed images will be downloaded the following locations:
+#  - DEST_LOCATION/opened
+#  - DEST_LOCATION/closed
+
+# The script is using the following commands:
+#   - rsync: To download the images from the server.
+#   - ssh: To connect to the server.
+
+DEST_LOCATION=data/01_raw/camera-images
+SSH_SERVER=192.168.1.6
+SSH_USER=hassio
+SOURCE_LOCATION=/config/camera-images
+OPENED_IMAGES_PATTERN="opened_*.jpg"
+CLOSED_IMAGES_PATTERN="closed_*.jpg"
+
+# Download the opened images.
+rsync -avz --remove-source-files -e ssh ${SSH_USER}@${SSH_SERVER}:${SOURCE_LOCATION}/${OPENED_IMAGES_PATTERN} ${DEST_LOCATION}/opened
+
+# Download the closed images.
+rsync -avz --remove-source-files -e ssh ${SSH_USER}@${SSH_SERVER}:${SOURCE_LOCATION}/${CLOSED_IMAGES_PATTERN} ${DEST_LOCATION}/closed
---
+++ src/opened_gate/train.py
@@ -0,0 +1,157 @@
+import lightning
+import torch
+import torchmetrics
+import torchvision
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+
+class ImagesDataModule(lightning.LightningDataModule):
+    def __init__(self, data_dir: str, batch_size=32):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],  # RGB
+                    std=[0.229, 0.224, 0.225],  # RGB
+                ),
+            ]
+        )
+
+    def prepare_data(self):
+        # download, split, etc...
+        # only called on 1 GPU/TPU in distributed
+        # do not assign state (self.x = y)
+        pass
+
+    def setup(self, stage):
+        # make assignments here (val/train/test split)
+        # called on every process in DDP
+        # do not assign state (self.x = y)
+        dataset = ImageFolder(self.data_dir, transform=self.transform)
+
+        # Split dataset into train (80%), val (10%), test (10%)
+        train_size = int(0.8 * len(dataset))
+        val_size = int(0.1 * len(dataset))
+        test_size = len(dataset) - train_size - val_size
+        self.train_dataset, self.val_dataset, self.test_dataset = random_split(
+            dataset, [train_size, val_size, test_size]
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
+
+
+class ClassificationModule(lightning.LightningModule):
+    def __init__(self, num_classes: int, backbone: str, learning_rate: float):
+        super().__init__()
+
+        # log hyperparameters
+        self.save_hyperparameters()
+
+        self.num_classes = num_classes
+        self.learning_rate = learning_rate
+
+        # Define model
+        if backbone == "resnet18":
+            self.backbone = torchvision.models.resnet18(pretrained=True)
+            self.backbone.fc = torch.nn.Linear(512, num_classes)
+        else:
+            raise NotImplementedError
+
+        self.model = self.backbone
+
+        # Define loss function
+        self.loss = torch.nn.CrossEntropyLoss()
+
+        # Define metrics
+        self.accuracy = torchmetrics.Accuracy(
+            task="multiclass", num_classes=num_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss(logits, y)
+
+        # Log metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss(logits, y)
+
+        # Log metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss(logits, y)
+
+        # Log metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", acc, prog_bar=True)
+        return loss
+
+
+def main():
+
+    # Create data module
+    data = ImagesDataModule(data_dir="data/01_raw/camera-images", batch_size=2)
+
+    # Create model
+    model = ClassificationModule(
+        num_classes=2,
+        backbone="resnet18",
+        learning_rate=0.001,
+    )
+
+    # Create trainer
+    trainer = lightning.Trainer(max_epochs=10)
+
+    # Train the model
+    trainer.fit(model, data)
+
+    # Test the model
+    trainer.test(model, data)
+
+    # Save the model
+    trainer.save_checkpoint("data/06_models/model.ckpt")
+    trainer.save_checkpoint("s3://opened-gate/models/model.ckpt")
+
+
+if __name__ == "__main__":
+    main()
